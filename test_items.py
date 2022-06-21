535787e82a0f275e28e2831c80d02985c87bbc58
Angelina <linochka@MacBook-Pro-Angelina.local>
2022-06-21 20:49:58 +0300

    unused imported modules deleted

--- test_items.py
+++ test_items.py
@@ -1,10 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-import math
+
 
 # Предположим, что у нас будет еще много тестов для Product Page :)
 class TestProductPage():
